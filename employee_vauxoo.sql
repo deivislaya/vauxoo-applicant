5c95e531633f453a90db2930694544c7031f3a92
deivislaya <deivislaya@gmail.com>
2015-12-19 13:22:09 -0430

    Aqui estoy creando tabla y los datos

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,46 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+  id serial NOT NULL,
+  first_name character varying,
+  last_name character varying
 );
 
+ALTER TABLE employee
+  ADD CONSTRAINT pkey_id PRIMARY KEY (id);
+
 CREATE TABLE employee_department (
+  id serial NOT NULL,
+  name character varying,
+  description character varying
 );
+
+ALTER TABLE employee_department
+  ADD CONSTRAINT pkey_departament PRIMARY KEY (id);
 
 CREATE TABLE employee_hobby (
 );
+
+
+ALTER TABLE employee ADD COLUMN departament_id integer;
+
+ALTER TABLE employee
+  ADD CONSTRAINT fkey_departament FOREIGN KEY (departament_id) REFERENCES employee_department (id)
+   ON UPDATE CASCADE ON DELETE CASCADE;
+CREATE INDEX fki_fkey_departament
+  ON employee(departament_id);
+
+
+INSERT INTO employee_department (name,description) VALUES ('Administracion', 'Administracion Financiera');
+INSERT INTO employee_department (name,description) VALUES ('Compras', 'Compras de oficina');
+INSERT INTO employee_department (name,description) VALUES ('Presupesto', 'Proyeccion de Presupuesto Anual');
+INSERT INTO employee_department (name,description) VALUES ('Produccion', 'Desarrollo de Nuevos Productos');
+INSERT INTO employee_department (name,description) VALUES ('Talento Humano', 'Administracion de Personal');
+INSERT INTO employee_department (name,description) VALUES ('Tecnologia', 'Administracion de Computadores');
+
+INSERT INTO employee (first_name, last_name, departament_id) VALUES ('Deivis', 'Laya', 1);
+INSERT INTO employee (first_name, last_name, departament_id) VALUES ('Yenifer', 'Santiago', 2);
+INSERT INTO employee (first_name, last_name, departament_id) VALUES ('Hernan', 'Neuman', 3);
+INSERT INTO employee (first_name, last_name, departament_id) VALUES ('Edgar', 'Camejo', 4);
 
 -- ...
