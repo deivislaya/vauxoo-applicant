673069c4baa3a28cd7dfe5c355cae5b80780d22a
deivislaya <deivislaya@gmail.com>
2015-12-19 14:42:51 -0430

    Agrego los Jefe a los empleados a la relacion empleados

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -28,6 +28,9 @@ CREATE TABLE employee_hobby (
 );
 
 
+ALTER TABLE employee_hobby
+  ADD CONSTRAINT pkey_hobby PRIMARY KEY (id);
+
 ALTER TABLE employee ADD COLUMN departament_id integer;
 
 ALTER TABLE employee
@@ -44,6 +47,7 @@ INSERT INTO employee_department (name,description) VALUES ('Produccion', 'Desarr
 INSERT INTO employee_department (name,description) VALUES ('Talento Humano', 'Administracion de Personal');
 INSERT INTO employee_department (name,description) VALUES ('Tecnologia', 'Administracion de Computadores');
 
+
 INSERT INTO employee_hobby (name,description) VALUES ('Cine', 'Peliculas');
 INSERT INTO employee_hobby (name,description) VALUES ('Playa', 'Cayo Sombrero');
 INSERT INTO employee_hobby (name,description) VALUES ('Video Juego', 'Warcraft');
@@ -53,6 +57,9 @@ CREATE TABLE employee_hobby_relation (
   employee_id integer,
   hobby_id integer
 );
+
+ALTER TABLE employee_hobby_relation
+  ADD CONSTRAINT pkey_hobby_rel PRIMARY KEY (id);
 
 
 INSERT INTO employee_hobby_relation (employee_id, hobby_id) VALUES (1,1);
@@ -64,5 +71,14 @@ INSERT INTO employee_hobby_relation (employee_id, hobby_id) VALUES (3,3);
 INSERT INTO employee_hobby_relation (employee_id, hobby_id) VALUES (4,1);
 INSERT INTO employee_hobby_relation (employee_id, hobby_id) VALUES (4,3);
 
+
+ALTER TABLE employee
+   ADD COLUMN jefe_id integer;
+
+ALTER TABLE employee
+  ADD CONSTRAINT fky_jefe FOREIGN KEY (jefe_id) REFERENCES employee (id)
+   ON UPDATE NO ACTION ON DELETE NO ACTION;
+CREATE INDEX fki_fky_jefe
+  ON employee(jefe_id);
 
 -- ...
