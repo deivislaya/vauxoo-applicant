d898e75bf9e6dc9a1dab40ea17973c5a1f3a01a7
deivislaya <deivislaya@gmail.com>
2015-12-19 17:31:44 -0430

    Se crea la funcion para verificar si un numero es primo en la clase is_primo

--- primes.py
+++ primes.py
@@ -5,12 +5,25 @@ Your module documentation here
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Your class documentation here.
     """
-
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Funcion para verificar si es primo.
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        primo = True
+        if isinstance(num_int, int):
+            # si es menos que 2 no es primo, por lo tanto devolvera Falso
+            if num_int < 2:
+                primo = False
+                return primo
+            # un rango desde el dos hasta el numero que nosotros elijamos
+            for inter in range(2, num_int):
+                # si el resto da 0 no es primo, por lo tanto devuelve Falso
+                if num_int % inter == 0:
+                    primo = False
+                    return primo
+        else:
+            primo = False
+            return primo
+        return primo    # de lo contrario devuelve Verdadero
